a5dfe16b01dc22d11d0a2df45b451ddf4c55c29b
samariddin-08 <samariddinruziqulov0@gmail.com>
2025-09-15 17:26:58 +0500

    Initial commit

---
+++ src/main/java/name/geadezest/GeadezestApplication.java
@@ -0,0 +1,13 @@
+package name.geadezest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeadezestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeadezestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/name/geadezest/GeadezestApplicationTests.java
@@ -0,0 +1,13 @@
+package name.geadezest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeadezestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
